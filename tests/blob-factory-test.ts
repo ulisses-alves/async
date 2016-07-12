a1ed89580b452df2c4d1e0d380c8ad4b0f21786b
Ulisses Bini de Paiva Alves <ulissesbpalves@gmail.com>
2016-07-12 15:46:55 -0300

    add tests for blob factory

---
+++ tests/blob-factory-test.ts
@@ -0,0 +1,19 @@
+import {expect} from 'chai'
+import * as async from '../src/async.d.ts'
+import BlobFactory from '../src/blob-factory'
+
+describe('BlobFactory', () => {
+  let blobFactory: async.BlobFactory
+
+  beforeEach(() => blobFactory = BlobFactory())
+
+  describe('call', () => {
+    let result: Blob
+
+    beforeEach(() => result = blobFactory([], {type: 'type/test'}))
+
+    it('should return a Blob', () => {
+      expect(result).to.be.a('Blob')
+    })
+  })
+})
